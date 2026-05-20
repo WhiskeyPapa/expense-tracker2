1ecf0766b390da3f3ac870d41184d83286fbd077
WhiskeyPapa <155653959+WhiskeyPapa@users.noreply.github.com>
2026-05-20 09:08:25 -0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'expenses-v1';
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.add('./index.html')));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(
+      fetch(e.request).catch(() => caches.match('./index.html'))
+    );
+  }
+});
